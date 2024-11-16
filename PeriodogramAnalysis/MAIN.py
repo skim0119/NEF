bc7e61daff11fc923b6b12407172837a8202e7dc
Qixian Wang <wqx1377823969@outlook.com>
2024-11-15 18:32:40 -0600

    add all psd methods

--- PeriodogramAnalysis/MAIN.py
+++ PeriodogramAnalysis/MAIN.py
@@ -6,6 +6,7 @@ from PowerSpectrumAnalysis import SpectrumAnalysis
 from miv.datasets.openephys_sample import load_data
 from miv.signal.filter import ButterBandpass, MedianFilter
 from miv.core.operator import Operator, DataLoader
+from Power_Spectral_Density import SpectrumAnalysisPeriodogram, SpectrumAnalysisMultitaper, SpectrumAnalysisWelch
 
 working_directory = "results"
 path: str = "/Users/aia/Downloads/2024-08-25_19-49-12"
@@ -15,6 +16,9 @@ print('file path:', path)
 dataset: DataManager = DataManager(data_collection_path=path)
 data: DataLoader = dataset[0]
 
+spectrum_welch = SpectrumAnalysisWelch()
+spectrum_per = SpectrumAnalysisPeriodogram()
+spectrum_mul = SpectrumAnalysisMultitaper()
 Periodogram_Analysis = PeriodogramAnalysis(
         window_length_for_welch=4
     )
@@ -25,10 +29,21 @@ Spectrum_Analysis = SpectrumAnalysis(
         band_display = [0, 5]
     )
 
-data >> Periodogram_Analysis
-data >> Spectrum_Analysis
-pipeline1 = Pipeline(Periodogram_Analysis)
-pipeline2 = Pipeline(Spectrum_Analysis)
+data >> spectrum_welch
+data >> spectrum_per
+data >> spectrum_mul
+pipeline1 = Pipeline(spectrum_welch)
+pipeline2 = Pipeline(spectrum_per)
+pipeline3 = Pipeline(spectrum_mul)
 pipeline1.run(working_directory=working_directory, verbose=True)
 pipeline2.run(working_directory=working_directory, verbose=True)
+pipeline3.run(working_directory=working_directory, verbose=True)
+
+
+# data >> Periodogram_Analysis
+# data >> Spectrum_Analysis
+# pipeline1 = Pipeline(Periodogram_Analysis)
+# pipeline2 = Pipeline(Spectrum_Analysis)
+# pipeline1.run(working_directory=working_directory, verbose=True)
+# pipeline2.run(working_directory=working_directory, verbose=True)
 
--- PeriodogramAnalysis/Power_Spectral_Density.py
+++ PeriodogramAnalysis/Power_Spectral_Density.py
@@ -1,16 +1,24 @@
+import os
+
+import pathlib
 import numpy as np
+import matplotlib.pyplot as plt
 from scipy import signal as sig
+from scipy.integrate import simpson
+from dataclasses import dataclass, field
 from collections import defaultdict
-from typing import Dict, Any
+from typing import Tuple, Dict, Any, Generator, Optional
 
 from miv.core.operator.operator import OperatorMixin
 from miv.core.operator.wrapper import cache_call
 from miv.core.datatype import Signal
 from miv.typing import SignalType
+from MultitaperPowerSpectrum import multitaper_psd
 
+@dataclass
 class SpectrumAnalysisWelch(OperatorMixin):
     window_length_for_welch: int = 4
-
+    band_display: Tuple[float, float] = field(default_factory=lambda: [0, 100])
     tag: str = "Welch PSD"
 
     def __post_init__(self):
@@ -23,14 +31,16 @@ class SpectrumAnalysisWelch(OperatorMixin):
 
         psd_dict: Dict[int, Dict[int, Dict[str, Any]]] = defaultdict(dict)
         for chunk_idx, signal_piece in enumerate(signal):
-            # Compute psd_dict and power_dict for welch_periodogram plotting
+            if chunk_idx >= 1:
+                break
+            signal_piece.data = signal_piece.data[:, [2]]
             psd_dict[chunk_idx] = self.spectrum_analysis_welch(signal_piece)
 
         return psd_dict
 
     def spectrum_analysis_welch(self, signal: Signal) -> Dict[int, Dict[str, Any]]:
         """
-        Compute Welch's periodogram for the signal.
+        Compute PSD using Welch's method for the signal.
 
         Parameters:
         -----------
@@ -52,3 +62,212 @@ class SpectrumAnalysisWelch(OperatorMixin):
             )
             psd_welch_dict[channel] = {"freqs": freqs, "psd": psd}
         return psd_welch_dict
+
+
+    def plot_spectrum_methods_welch(
+            self,
+            output,
+            input,
+            show: bool = False,
+            save_path: Optional[pathlib.Path] = None,
+    ):
+
+        psd_dict = output
+        for chunk in psd_dict.keys():
+            for channel in psd_dict[chunk].keys():
+                plt.stem(
+                    psd_dict[chunk][channel]["freqs"],
+                    psd_dict[chunk][channel]["psd"],
+                    linefmt="slategrey",
+                    basefmt=" ",
+                    markerfmt=" ",
+                )
+                plt.plot(
+                    psd_dict[chunk][channel]["freqs"],
+                    psd_dict[chunk][channel]["psd"],
+                    lw=0.5,
+                    color="k",
+                )
+                plt.xlabel("Frequency (Hz)")
+                plt.ylabel("Decibels (dB / Hz)")
+                plt.title("Welch")
+                plt.xlim(self.band_display)
+                plt.ylim(ymin=0)
+
+                if show:
+                    plt.show()
+                if save_path is not None:
+                    plot_path = os.path.join(
+                        save_path,
+                        f"Chunk{chunk}_welch_channel_{channel}.png",
+                    )
+                    plt.savefig(plot_path, dpi=300)
+                plt.close()
+
+@dataclass
+class SpectrumAnalysisPeriodogram(OperatorMixin):
+    band_display: Tuple[float, float] = field(default_factory=lambda: [0, 100])
+    tag: str = "Periodogram PSD"
+
+    def __post_init__(self):
+        super().__init__()
+
+    @cache_call
+    def __call__(
+        self, signal: SignalType
+    ) -> Dict[int, Dict[int, Dict[str, Any]]]:
+
+        psd_dict: Dict[int, Dict[int, Dict[str, Any]]] = defaultdict(dict)
+        for chunk_idx, signal_piece in enumerate(signal):
+            if chunk_idx >= 1:
+                break
+            signal_piece.data = signal_piece.data[:, [2]]
+            psd_dict[chunk_idx] = self.spectrum_analysis_periodogram(signal_piece)
+
+        return psd_dict
+
+    def spectrum_analysis_periodogram(self, signal: Signal) -> Dict[int, Dict[str, Any]]:
+        """
+        Compute PSD using periodogram for the signal.
+
+        Parameters:
+        -----------
+        signal : SignalType
+            Input signal to be analyzed.
+
+        Returns:
+        --------
+        psd_periodogram_dict
+            Dictionary containing PSD values for all channels in this chunk.
+        """
+        psd_periodogram_dict: Dict[int, Dict[str, Any]] = defaultdict(dict)
+
+        for channel in range(signal.number_of_channels):
+            signal_no_bias = signal.data[:, channel] - np.mean(signal.data[:, channel])
+            freqs, psd = sig.periodogram(signal_no_bias, signal.rate)
+            psd_periodogram_dict[channel] = {"freqs": freqs, "psd": psd}
+        return psd_periodogram_dict
+
+    def plot_spectrum_methods_periodogram(
+            self,
+            output,
+            input,
+            show: bool = False,
+            save_path: Optional[pathlib.Path] = None,
+    ):
+
+        psd_dict = output
+        for chunk in psd_dict.keys():
+            for channel in psd_dict[chunk].keys():
+                plt.stem(
+                    psd_dict[chunk][channel]["freqs"],
+                    psd_dict[chunk][channel]["psd"],
+                    linefmt="slategrey",
+                    basefmt=" ",
+                    markerfmt=" ",
+                )
+                plt.plot(
+                    psd_dict[chunk][channel]["freqs"],
+                    psd_dict[chunk][channel]["psd"],
+                    lw=0.5,
+                    color="k",
+                )
+                plt.xlabel("Frequency (Hz)")
+                plt.ylabel("Decibels (dB / Hz)")
+                plt.title("Periodogram")
+                plt.xlim(self.band_display)
+                plt.ylim(ymin=0)
+
+                if show:
+                    plt.show()
+                if save_path is not None:
+                    plot_path = os.path.join(
+                        save_path,
+                        f"Chunk{chunk}_periodogram_channel_{channel}.png",
+                    )
+                    plt.savefig(plot_path, dpi=300)
+                plt.close()
+
+@dataclass
+class SpectrumAnalysisMultitaper(OperatorMixin):
+    band_display: Tuple[float, float] = field(default_factory=lambda: [0, 100])
+    tag: str = "Multitaper PSD"
+
+    def __post_init__(self):
+        super().__init__()
+
+    @cache_call
+    def __call__(
+        self, signal: SignalType
+    ) -> Dict[int, Dict[int, Dict[str, Any]]]:
+
+        psd_dict: Dict[int, Dict[int, Dict[str, Any]]] = defaultdict(dict)
+        for chunk_idx, signal_piece in enumerate(signal):
+            if chunk_idx >= 1:
+                break
+            signal_piece.data = signal_piece.data[:, [2]]
+            psd_dict[chunk_idx] = self.spectrum_analysis_multitaper(signal_piece)
+
+        return psd_dict
+
+    def spectrum_analysis_multitaper(self, signal: Signal) -> Dict[int, Dict[str, Any]]:
+        """
+        Compute Welch's periodogram for the signal.
+
+        Parameters:
+        -----------
+        signal : SignalType
+            Input signal to be analyzed.
+
+        Returns:
+        --------
+        psd_multitaper_dict
+            Dictionary containing PSD values for all channels in this chunk.
+        """
+        psd_multitaper_dict: Dict[int, Dict[str, Any]] = defaultdict(dict)
+
+        for channel in range(signal.number_of_channels):
+            signal_no_bias = signal.data[:, channel] - np.mean(signal.data[:, channel])
+            psd, freqs = multitaper_psd(signal_no_bias, signal.rate)
+            psd_multitaper_dict[channel] = {"freqs": freqs, "psd": psd}
+        return psd_multitaper_dict
+
+    def plot_spectrum_methods_multitaper(
+        self,
+        output,
+        input,
+        show: bool = False,
+        save_path: Optional[pathlib.Path] = None,
+    ):
+
+        psd_dict = output
+        for chunk in psd_dict.keys():
+            for channel in psd_dict[chunk].keys():
+                plt.stem(
+                    psd_dict[chunk][channel]["freqs"],
+                    psd_dict[chunk][channel]["psd"],
+                    linefmt="slategrey",
+                    basefmt=" ",
+                    markerfmt=" ",
+                )
+                plt.plot(
+                    psd_dict[chunk][channel]["freqs"],
+                    psd_dict[chunk][channel]["psd"],
+                    lw=0.5,
+                    color="k",
+                )
+                plt.xlabel("Frequency (Hz)")
+                plt.ylabel("Decibels (dB / Hz)")
+                plt.title("Multitaper")
+                plt.xlim(self.band_display)
+                plt.ylim(ymin=0)
+
+                if show:
+                    plt.show()
+                if save_path is not None:
+                    plot_path = os.path.join(
+                        save_path,
+                        f"Chunk{chunk}_multitaper_channel_{channel}.png",
+                    )
+                    plt.savefig(plot_path, dpi=300)
+                plt.close()
