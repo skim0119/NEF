1eca39c005eed9538a46e31c61a73770a773d70d
Qixian-Wang <wqx1377823969@outlook.com>
2024-11-26 18:14:27 -0600

    polish and reformat

--- controllability/controllability_test_1.py
+++ controllability/controllability_test_1.py
@@ -2,6 +2,7 @@ import networkx as nx
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 # WRG gaussian weight added
 def weighted_random(n, p, mean, std):
     graph = nx.erdos_renyi_graph(n, p)
@@ -79,7 +80,9 @@ def random_geometric(n, p, mean, std):
     gaussian_weights = np.random.normal(mean, std, len(graph.edges()))
     gaussian_weights.sort()
 
-    for (old_weight, old_dist, u, v), new_weight in zip(weight_list[:k], gaussian_weights):
+    for (old_weight, old_dist, u, v), new_weight in zip(
+        weight_list[:k], gaussian_weights
+    ):
         graph[u][v]["weight"] = new_weight
 
     return graph
@@ -109,7 +112,7 @@ def compute_controllability(graph_type, num_nodes, num_iter):
     ave_controllability_list2 = []
     modal_controllability = np.zeros((num_iter, num_nodes))
 
-    for i in range(num_iter):
+    for iter in range(num_iter):
         if graph_type == "WRG":
             graph = weighted_random(num_nodes, 0.2, mean=0.5, std=0.12)
         elif graph_type == "RL":
@@ -125,7 +128,9 @@ def compute_controllability(graph_type, num_nodes, num_iter):
         mat_adjacent = nx.adjacency_matrix(graph).todense()
         eigenvalues_adjacent, eigenvectors_adjacent = np.linalg.eig(mat_adjacent)
         mat_adjacent_re = mat_adjacent / (1 + np.max(eigenvalues_adjacent))
-        eigenvalues_adjacent_re, eigenvectors_adjacent_re = np.linalg.eig(mat_adjacent_re)
+        eigenvalues_adjacent_re, eigenvectors_adjacent_re = np.linalg.eig(
+            mat_adjacent_re
+        )
         eigenvalues_adjacent_squared = np.abs(eigenvalues_adjacent_re) ** 2
 
         global_controllability_list = []
@@ -145,88 +150,98 @@ def compute_controllability(graph_type, num_nodes, num_iter):
                     ]
                 )
                 rank = np.linalg.matrix_rank(controllability_matrix)
-                if rank == step:
-                    continue
-                else:
+                if rank < step:
                     max_step = step
                     break
 
             # Compute controllability grammian
             grammian = np.zeros((num_nodes, num_nodes))
-            adjacency_t = np.eye(num_nodes)
             for t in range(max_step):
                 adjacency_t = np.linalg.matrix_power(mat_adjacent_re, t)
                 grammian += adjacency_t @ mat_input @ mat_input.T @ adjacency_t.T
 
-            # Ensure it is symmetric
+            # Compute global con
             grammian = (grammian + grammian.T) / 2
             eigenvals_wc, _ = np.linalg.eigh(grammian)
             global_controllability_list.append(np.min(abs(eigenvals_wc)))
 
+            # Compute average con
             mat_unit = np.eye(mat_adjacent_re.shape[0])
             mat_b = mat_input @ mat_input.T
             mat_a = np.linalg.inv(mat_unit - np.linalg.matrix_power(mat_adjacent_re, 2))
             ave_controllability_list.append(np.trace(mat_a @ mat_b))
 
+            # Compute modal con
             phi = 0
             for mode in range(num_nodes):
                 phi += (1 - eigenvalues_adjacent_squared[mode]) * (
-                        eigenvectors_adjacent_re[node, mode] ** 2
+                    eigenvectors_adjacent_re[node, mode] ** 2
                 )
-            modal_controllability[i, node] = phi
+            modal_controllability[iter, node] = phi
 
         global_controllability_list2.append(np.mean(global_controllability_list))
         ave_controllability_list2.append(np.mean(ave_controllability_list))
 
     modal_controllability_ave = np.mean(modal_controllability, axis=1)
 
-    # Compute global controllability
     print(np.max(global_controllability_list2))
     print(np.min(global_controllability_list2))
 
-    # Compute average controllability
     print(np.max(ave_controllability_list2))
     print(np.min(ave_controllability_list2))
 
-    # Compute modal controllability
     print(np.max(modal_controllability_ave))
     print(np.min(modal_controllability_ave))
 
-    return global_controllability_list2, ave_controllability_list2, modal_controllability_ave
+    return (
+        global_controllability_list2,
+        ave_controllability_list2,
+        modal_controllability_ave,
+    )
 
 
 num_nodes = 128
-num_iter = 50
+num_iter = 25
 
-WRG_glo, WRG_ave, WRG_mod = compute_controllability(graph_type="WRG", num_nodes=num_nodes, num_iter=num_iter)
-RL_glo, RL_ave, RL_mod = compute_controllability(graph_type="WRG", num_nodes=num_nodes, num_iter=num_iter)
-WS_glo, WS_ave, WS_mod = compute_controllability(graph_type="WRG", num_nodes=num_nodes, num_iter=num_iter)
-RG_glo, RG_ave, RG_mod = compute_controllability(graph_type="WRG", num_nodes=num_nodes, num_iter=num_iter)
-BA_glo, BA_ave, BA_mod = compute_controllability(graph_type="WRG", num_nodes=num_nodes, num_iter=num_iter)
+WRG_glo, WRG_ave, WRG_mod = compute_controllability(
+    graph_type="WRG", num_nodes=num_nodes, num_iter=num_iter
+)
+RL_glo, RL_ave, RL_mod = compute_controllability(
+    graph_type="WRG", num_nodes=num_nodes, num_iter=num_iter
+)
+WS_glo, WS_ave, WS_mod = compute_controllability(
+    graph_type="WRG", num_nodes=num_nodes, num_iter=num_iter
+)
+RG_glo, RG_ave, RG_mod = compute_controllability(
+    graph_type="WRG", num_nodes=num_nodes, num_iter=num_iter
+)
+BA_glo, BA_ave, BA_mod = compute_controllability(
+    graph_type="WRG", num_nodes=num_nodes, num_iter=num_iter
+)
 
 data1 = [WRG_glo, RL_glo, WS_glo, RG_glo, BA_glo]
 data2 = [WRG_ave, RL_ave, WS_ave, RG_ave, BA_ave]
 data3 = [WRG_mod, RL_mod, WS_mod, RG_mod, BA_mod]
-labels = ['WRG', 'RL', 'WS', 'RG', 'BA']
+labels = ["WRG", "RL", "WS", "RG", "BA"]
 
 plt.figure(1, figsize=(10, 6))
 plt.boxplot(data1, patch_artist=True, showmeans=True)
 plt.xticks(range(1, len(labels) + 1), labels)
-plt.ylabel(r'Global Controllability ($\times 10^{-21}$)')
-plt.title('Gaussian, 128 nodes')
-plt.savefig('Global Controllability.png', dpi=300)
+plt.ylabel(r"Global Controllability ($\times 10^{-21}$)")
+plt.title("Gaussian, 128 nodes")
+plt.savefig("Global Controllability.png", dpi=300)
 
 plt.figure(2, figsize=(10, 6))
 plt.boxplot(data2, patch_artist=True, showmeans=True)
 plt.xticks(range(1, len(labels) + 1), labels)
-plt.ylabel(r'Average Controllability ($\times 10^{-21}$)')
-plt.title('Gaussian, 128 nodes')
-plt.savefig('Average Controllability.png', dpi=300)
+plt.ylabel(r"Average Controllability ($\times 10^{-21}$)")
+plt.title("Gaussian, 128 nodes")
+plt.savefig("Average Controllability.png", dpi=300)
 
 plt.figure(3, figsize=(10, 6))
 plt.boxplot(data3, patch_artist=True, showmeans=True)
 plt.xticks(range(1, len(labels) + 1), labels)
-plt.ylabel(r'Modal Controllability ($\times 10^{-21}$)')
-plt.title('Gaussian, 128 nodes')
-plt.savefig('Modal Controllability.png', dpi=300)
+plt.ylabel(r"Modal Controllability ($\times 10^{-21}$)")
+plt.title("Gaussian, 128 nodes")
+plt.savefig("Modal Controllability.png", dpi=300)
 plt.show()
